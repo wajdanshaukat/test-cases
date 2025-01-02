87f8b3a4a38d4b1456601f24ccdca33c494bbf4d
wajdanshaukat <wajdanshaukat927@gmail.com>
2025-01-02 11:22:59 +0500

    frontend test cases using jest library and further working on it

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>person</h1>
+     <p>hello</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/sum.test.js
@@ -0,0 +1,9 @@
+import sum from "./sum";
+
+test("testing for sum function", () => {
+    expect(sum(10, 10)).toBe(20)
+})
+
+test("testing for sum function", () => {
+    expect(sum(15, 15)).toBe(30)
+})
